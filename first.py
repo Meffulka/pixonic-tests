98ac77b9f0f9d6bc4b5ed14979adaa8029331a86
Alekseii Erokhin <erokhin@action-media.ru>
2020-10-01 14:05:57 +0300

    add port checking

--- first.py
+++ first.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
 import os
+import socket
 
 nginxStatus = os.system('service nginx status  > /dev/null 2>&1')
 
 if nginxStatus == 0:
     nginxConfTest = os.system('nginx -t > /dev/null 2>&1')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketResult = sock.connect_ex(('127.0.0.1',80))
     if nginxConfTest == 0:
+        print("Status: OK")
+    elif socketResult == 0:
         print("Status: OK")
     else:
         print("Status: Warning")
